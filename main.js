c6fc932865b01a5009918fbe1c293b14441a817c
Feddy <feddy@MacBook-Air-Feddy.local>
2022-04-01 23:26:17 +0700

    init JS

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
-class Game {
-  constructor(player, com, result) {
-    this.player = player;
-    this.com = com;
-    this.result = result;
+const pickUserHand = (hand) => {
+  let player = document.querySelector(".player");
+  player.style.display = "none";
+
+  pickComputerHand(hand);
+};
+
+const pickComputerHand = (hand) => {
+  let player = ["rock", "paper", "scissors"];
+  let com = player[Math.floor(Math.random() * 3)];
+  referee(hand, com);
+};
+
+const referee = (player, com) => {
+  if (player == "paper" && com == "scissors") {
+    setDecision("YOU LOSE!");
   }
-}
+  if (player == "paper" && com == "rock") {
+    setDecision("YOU WIN!");
+  }
+  if (player == "paper" && com == "paper") {
+    setDecision("It's a tie!");
+  }
+  if (player == "rock" && com == "scissors") {
+    setDecision("YOU WIN!");
+  }
+  if (player == "rock" && com == "paper") {
+    setDecision("YOU LOSE!");
+  }
+  if (player == "rock" && com == "rock") {
+    setDecision("It's a tie!");
+  }
+  if (player == "scissors" && com == "scissors") {
+    setDecision("It's a tie!");
+  }
+  if (player == "scissors" && com == "rock") {
+    setDecision("YOU LOSE!");
+  }
+  if (player == "scissors" && com == "paper") {
+    setDecision("YOU WIN!");
+  }
+};
+
+const setDecision = (decision) => {
+  document.querySelector(".decision").innerText = decision;
+};
